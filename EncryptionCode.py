9d9054afaed6dde5eb28ecf10f4553d58ac5a228
SirChesus <mayankattela@gmail.com>
2024-04-16 17:42:32 -0700

    slight mo

--- EncryptionCode.py
+++ EncryptionCode.py
@@ -5,6 +5,10 @@ class EncryptionCode():
     self.shift = shift
     self.proper = proper
     self.password = password
+    self.cipher2 = {'a': 'ఝ', 'b': 'ఈ', 'c': 'ఞ', 'd': 'న', 'e': 'ఠ', 'f': 'ౡ', 'g': 'భ',
+            'h': 'శ', 'i': 'ౙ', 'j': 'క', 'k': 'ఊ', 'l': 'ణ', 'm': 'ప', 'n': 'మ',
+            'o': 'జై', 'p': 'హా', 'q': 'లో', 'r': 'రు', 's': 'ఓం', 't': 'ౘ', 'u':'ఓ' ,
+            'v': 'ట్టే', 'w': 'లు', 'x': 'పొ', 'y': 'గ్రా', 'z': 'బొ'}
 #-------------------------------------------------------------------------------
   def get_isProper(self):
     return self.proper
@@ -25,17 +29,12 @@ class EncryptionCode():
     return str(joint)
 #-------------------------------------------------------------------------------
   def caesarAdv(self, string, shift):
-    cipher2 = {'a': 'ఝ', 'b': 'ఈ', 'c': 'ఞ', 'd': 'న', 'e': 'ఠ', 'f': 'ౡ', 'g': 'భ',
-                'h': 'శ', 'i': 'ౙ', 'j': 'క', 'k': 'ఊ', 'l': 'ణ', 'm': 'ప', 'n': 'మ',
-                'o': 'జై', 'p': 'హా', 'q': 'లో', 'r': 'రు', 's': 'ఓం', 't': 'ౘ', 'u':'ఓ' ,
-                'v': 'ట్టే', 'w': 'లు', 'x': 'పొ', 'y': 'గ్రా', 'z': 'బొ'}
-
     if self.get_isProper:
       joint = ''
       for letter in range(len(string)):
         ascii = ord(string[letter:letter+1].lower())
         ascii = (shift + ascii) % 122
-        joint += ''.join(cipher2[chr(ascii)])
+        joint += ''.join(self.cipher2[chr(ascii)])
 
     else:
       return 'Improper!'
@@ -56,16 +55,11 @@ class EncryptionCode():
       return str(joint)
 #-------------------------------------------------------------------------------
   def keyAdv(self, string, shift, password):
-    cipher2 = {'a': 'ఝ', 'b': 'ఈ', 'c': 'ఞ', 'd': 'న', 'e': 'ఠ', 'f': 'ౡ', 'g': 'భ',
-            'h': 'శ', 'i': 'ౙ', 'j': 'క', 'k': 'ఊ', 'l': 'ణ', 'm': 'ప', 'n': 'మ',
-            'o': 'జై', 'p': 'హా', 'q': 'లో', 'r': 'రు', 's': 'ఓం', 't': 'ౘ', 'u':'ఓ' ,
-            'v': 'ట్టే', 'w': 'లు', 'x': 'పొ', 'y': 'గ్రా', 'z': 'బొ'}
-
     if password == 'sleepy':
       joint = ''
       telToEng = ''
       for letter in range(len(string)+1):
-        for eng, tel in cipher2.items():
+        for eng, tel in self.cipher2.items():
           if tel == string[letter:letter+1]:
             telToEng = eng
             break
